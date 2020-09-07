86c364e3329d381b76d2316f1cc3a92e0bead9be
jungho Hwang <junghohwang@ipageon.com>
2020-09-07 13:40:19 +0900

    푸쉬

--- gitAlias.sh
+++ gitAlias.sh
@@ -38,3 +38,15 @@ alias gdsn='git diff --staged --name-only'
 alias gdn='git diff --name-only'
 
 #alias ggpnp='git pull origin $(current_branch) && git push origin $(current_branch)'
+
+
+alias vi='nvim'
+
+
+git_branch() {
+	  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+  }
+
+export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \$(git_branch)\$ "
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+
