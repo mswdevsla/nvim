ace19b207e6c4e566fe8f8429f97ef58eaad903e
jungting20 <jungting20@gmail.com>
2022-10-29 00:51:12 +0900

    완료

--- init.lua
+++ init.lua
@@ -1,43 +1,43 @@
-require("jungho.plugins-setup") -- jungho/plugins-setup
+require("jungho.plugins-setup") -- lua/jungho/plugins-setup
 require("jungho.core.base") -- lua/jungho/core/base
-require("jungho.core.highlights") -- jungho/core/highlights
-require("jungho.core.keymaps") -- jungho/core/maps
-require("jungho.core.colorscheme") -- jungho/core/colorscheme
+require("jungho.core.highlights") -- lua/jungho/core/highlights
+require("jungho.core.keymaps") -- lua/jungho/core/maps
+require("jungho.core.colorscheme") -- lua/jungho/core/colorscheme
 
-require("jungho.plugins.autopairs") -- jungho/plugins/autopairs
-require("jungho.plugins.bufferline") -- jungho/plugins/bufferline require("jungho.plugins.cmp") -- jungho/plugins/cmp
-require("jungho.plugins.colorizer") -- jungho/plugins/colorizer
-require("jungho.plugins.web-devicons") -- jungho/plugins/web-devicons
+require("jungho.plugins.autopairs") -- lua/jungho/plugins/autopairs
+require("jungho.plugins.bufferline") -- lua/jungho/plugins/bufferline require("jungho.plugins.cmp") -- jungho/plugins/cmp
+require("jungho.plugins.colorizer") -- lua/jungho/plugins/colorizer
+require("jungho.plugins.web-devicons") -- lua/jungho/plugins/web-devicons
 
 --lsp
 -- require('jungho.plugins.lsp-colors')
-require("jungho.plugins.lspconfig") -- jungho/plugins/lspconfig
-require("jungho.plugins.lspkind") -- jungho/plugins/lspkind
-require("jungho.plugins.lspsaga") -- jungho/plugins/lspsaga
-require("jungho.plugins.mason") -- lsp 다운로드 하는 -- jungho/plugins/mason
-require("jungho.plugins.null-ls") -- jungho/plugins/null-ls
-require("jungho.plugins.prettier") -- jungho/plugins/prettier
+require("jungho.plugins.lspconfig") -- lua/jungho/plugins/lspconfig
+require("jungho.plugins.lspkind") -- lua/jungho/plugins/lspkind
+require("jungho.plugins.lspsaga") -- lua/jungho/plugins/lspsaga
+require("jungho.plugins.mason") -- lsp 다운로드 하는 -- lua/jungho/plugins/mason
+require("jungho.plugins.null-ls") -- lua/jungho/plugins/null-ls
+require("jungho.plugins.prettier") -- lua/jungho/plugins/prettier
 
-require("jungho.plugins.telescope") -- jungho/plugins/telescope
-require("jungho.plugins.treesitter") -- jungho/plugins/treesitter
+require("jungho.plugins.telescope") -- lua/jungho/plugins/telescope
+require("jungho.plugins.treesitter") -- lua/jungho/plugins/treesitter
 -- require('jungho.plugins.tokyonight')
-require("jungho.plugins.ts-autotag") -- jungho/plugins/ts-autotag
-require("jungho.plugins.web-devicons") -- jungho/plugins/web-devicons
-require("jungho.plugins.zen-mode") -- jungho/plugins/zen-mode
+require("jungho.plugins.ts-autotag") -- lua/jungho/plugins/ts-autotag
+require("jungho.plugins.web-devicons") -- lua/jungho/plugins/web-devicons
+require("jungho.plugins.zen-mode") -- lua/jungho/plugins/zen-mode
 
-require("jungho.plugins.lualine") -- jungho/plugins/lualine
-require("jungho.plugins.nvim-tree") -- jungho/plugins/nvim-tree
+require("jungho.plugins.lualine") -- lua/jungho/plugins/lualine
+require("jungho.plugins.nvim-tree") -- lua/jungho/plugins/nvim-tree
 
 -- git
-require("jungho.plugins.git") -- jungho/plugins/git
-require("jungho.plugins.gitsigns") -- jungho/plugins/lualine
-require("jungho.plugins.fugitive") -- jungho/plugins/fugitive
+require("jungho.plugins.git") -- lua/jungho/plugins/git
+require("jungho.plugins.gitsigns") -- lua/jungho/plugins/lualine
+require("jungho.plugins.fugitive") -- lua/jungho/plugins/fugitive
 
-require("jungho.plugins.comment") -- jungho/plugins/lualine
-require("jungho.plugins.surround") -- jungho/plugins/lualine
+require("jungho.plugins.comment") -- lua/jungho/plugins/comment
+require("jungho.plugins.surround") -- lua/jungho/plugins/surround
 
 -- require('jungho.plugins.neosolarized')
-require("jungho.plugins.packer_compiled") -- jungho/plugins/packer_compiled
+require("jungho.plugins.packer_compiled") -- lua/jungho/plugins/packer_compiled
 
 local has = vim.fn.has
 local is_mac = has("macunix")
