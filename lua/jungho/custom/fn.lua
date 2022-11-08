7aa62fa6614a015abc448683d9755a6d3f0dc3c1
jungho <jungting20@gmail.com>
2022-11-08 11:00:22 +0900

    install

--- lua/jungho/custom/fn.lua
+++ lua/jungho/custom/fn.lua
@@ -2,6 +2,8 @@ function inputTextCurrentCursor(text)
 	local pos = vim.api.nvim_win_get_cursor(0)[2]
 	local line = vim.api.nvim_get_current_line()
 	local nline = line:sub(0, pos) .. text .. line:sub(pos + 1)
+
+
 	vim.api.nvim_set_current_line(nline)
 end
 
